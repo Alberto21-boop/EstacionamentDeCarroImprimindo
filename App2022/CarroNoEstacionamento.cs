398a26185fbd9bf5418c8039174a31df5ed360ee
alberto <albertobarbosa0003@gmail.com>
2022-04-26 14:33:23 -0300

    Adicionar arquivos de projeto.

---
+++ App2022/CarroNoEstacionamento.cs
@@ -0,0 +1,20 @@
+﻿using App2022;
+using System.IO;
+
+Carros carros = new Carros();
+
+carros.Nome = Console.ReadLine();
+carros.Cor = Console.ReadLine();
+carros.LetrasDaPlaca = Console.ReadLine();
+carros.NumerosDaPlaca = int.Parse(Console.ReadLine());
+carros.QauntidadeDePortas = int.Parse(Console.ReadLine());
+
+string[] lines = { "Carro : " + carros.NomeDoCarro(), "Cor do Carro : " + carros.CorDoCarros(), "Placa Do Carro : " + carros.PlacaDoCarro(), "Portas do Carro : " + carros.PortasDoCarro() };
+
+string docPath = @"C:\Users\Àlberto Barbòsa\Desktop\PastaImprime";
+
+using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+{
+    foreach (string line in lines)
+        outputFile.WriteLine(line);
+}
---
+++ App2022/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace App2022
+{
+    internal class Carros
+    {
+        public string Nome { get; set; } = default!;
+        public string Cor { get; set; } = default!;
+        public string LetrasDaPlaca { get; set; } = default!;
+        public int NumerosDaPlaca { get; set; }
+        public int QauntidadeDePortas { get; set; }
+
+        public string NomeDoCarro()
+        {
+            return Nome;
+        }
+        public string CorDoCarros()
+        {
+            return Cor;
+        }
+        public string PlacaDoCarro()
+        {
+            return LetrasDaPlaca  + NumerosDaPlaca;
+        }
+        public int PortasDoCarro()
+        {
+            return QauntidadeDePortas;
+        }
+    }
+}
